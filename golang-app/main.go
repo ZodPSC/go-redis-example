b489d07c877215c91651008f526735383b3d644b
Dmitriy <d>
2024-12-18 19:10:52 +0300

    add get value and check life value on server

--- golang-app/main.go
+++ golang-app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/redis/go-redis/v9"
 	"golang-app/storage"
 	"time"
 )
@@ -28,5 +29,38 @@ func main() {
 		fmt.Printf("failed to set data, error: %s", err.Error())
 	}
 
-	fmt.Println("completed3.")
+	//30sec for life in DB
+	if err := db.Set(context.Background(), "key2", 111, 30*time.Second).Err(); err != nil {
+		fmt.Printf("failed to set data, error: %s", err.Error())
+	}
+
+	val, err := db.Get(context.Background(), "key").Result()
+	if err == redis.Nil {
+		fmt.Println("value not found")
+	} else if err != nil {
+		fmt.Printf("failed to get value, error: %v\n", err)
+	}
+
+	val2, err := db.Get(context.Background(), "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("value not found")
+	} else if err != nil {
+		fmt.Printf("failed to get value, error: %v\n", err)
+	}
+
+	fmt.Printf("value: %v\n", val)
+	fmt.Printf("value2: %v\n", val2)
+
+	time.Sleep(40 * time.Second)
+
+	oldVal2, err := db.Get(context.Background(), "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("value not found")
+	} else if err != nil {
+		fmt.Printf("failed to get value, error: %v\n", err)
+	}
+
+	fmt.Printf("after some time value : %v\n", oldVal2)
+
+	fmt.Println("completed.")
 }
